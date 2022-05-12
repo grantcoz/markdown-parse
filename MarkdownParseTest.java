e777731483c1c4337273509a7a9bc8f67f60c2c0
grantcoz <grantcozzens4444@gmail.com>
2022-05-12 16:43:50 -0700

    adding mdparse

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,11 +1,10 @@
 import static org.junit.Assert.*;
-
+import org.junit.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
-import org.junit.*;
 public class MarkdownParseTest {
     @Test
     public void addition() {
